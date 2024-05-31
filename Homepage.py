e6c060be5b120a5b3a95be2fca2d61c0bcf278ce
colemandavis1120 <colemandavis1120@gmail.com>
2024-05-31 16:04:12 -0500

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -2,42 +2,46 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.set_page_config(page_title='Huntsville MBB Performance Analysis')
-st.header('Performance Analysis 2023-2025')
+st.set_page_config(page_title='Huntsville MBB Performance Analysis',
+                   layout="wide", 
+                   page_icon="whitelogo.png",
+                   initial_sidebar_state="expanded")
+col1, col2 = st.columns([6, 1])
+with col1:
+    st.title("Huntsville HS MBB Performace Analysis")
+    st.header('2024-2025')
+    st.markdown("""
+    <style>
+    .big-font {font-size:13px !important;}
+</style>
+""", unsafe_allow_html=True)
+    
+st.markdown('<p class="big-font">Developed by Huntsville High School MBB Director of Scouting and Analytics: Coach Cole Davis</p>', unsafe_allow_html=True)
 
-excel_file = 'analytics_database.xlsx'
-sheet_name = 'DATA'
+with col2:
+    st.image("whitelogo.png", width=175)
+st.markdown("___")
+###################################################
 
-df = pd.read_excel(excel_file,
-                   sheet_name=sheet_name,
-                   usecols='A:R',
+col1, col2 = st.columns([2, 1])
+with col1:
+    st.subheader("How to understand and use advanced stats")
+
+excel_file1 = 'analytics_database.xlsx'
+sheet_name1 = 'homepagedata'
+sheet_name2 = 'homepagedata2'
+
+df1 = pd.read_excel(excel_file1,
+                   sheet_name=sheet_name2,
+                   usecols='A:B',
                    header=1)
+df2 = pd.read_excel(excel_file1,
+                    sheet_name=sheet_name1,
+                    usecols='A:Q',
+                    header=1)
 
+with col1:
+    st.dataframe(df1, hide_index=True)
+    st.subheader("Advanced Stats Season Averages")
+    st.dataframe(df2, hide_index=True)
 
-
-x_column = st.selectbox("Select X Axis", df.columns.tolist())
-y_column = st.selectbox("Select Y Axis", df.columns.tolist())
-
-player_filter = st.multiselect('Select Player', options=list(df['Player'].unique()), default=list(df['Player'].unique()))
-opponent_filter = st.multiselect('Select Opponent', options=list(df['Opponent'].unique()), default=list(df['Opponent'].unique()))
-filtered_data = df[df['Player'].isin(player_filter) & df['Opponent'].isin(opponent_filter)]
-
-st.subheader("Player Comparison Scatter Plot:")
-fig = px.scatter(filtered_data, 
-                 x=x_column, 
-                 y=y_column,
-                 hover_name='Player',
-                 hover_data={'Player': True},
-                 template='plotly',
-                 )   
-
-fig.update_traces(hovertemplate='%{hovertext}') 
-fig.update_layout(
-    hoverlabel=dict(
-        bgcolor="Blue", 
-        font_size=20,
-        font_color="White",
-    )
-)  
-
-st.plotly_chart(fig, use_container_width=True)
